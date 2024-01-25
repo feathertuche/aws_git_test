7629255264d75d6fbc272a41ce0f5484fc54c926
Khushal Kargutkar <146172246+khushalkarg@users.noreply.github.com>
2024-01-25 17:06:22 +0530

    Devops (#7)
    * Updated devops pipeline
    
    * Updated the dockerfile name
    
    * Removed the directory
    
    * Removed lambda
    
    * Updated the url patterns

--- merge_integration/urls.py
+++ merge_integration/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from .views import health_check
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('ACCOUNTS.urls')),
+    path('api/erp/admin/', admin.site.urls),
+    path('api/v1/erp/', include('ACCOUNTS.urls')),
     path('api/v1/', include('LINKTOKEN.urls')),
-    path('health/', health_check, name='health_check'),
+    path('api/v1/health/', health_check, name='health_check'),
 ]
