3b64a96a4282507c41528faafd34fcb93926dbbd
Dhirajgiri <dhiraj.giri@blenheimchalcot.com>
2024-01-29 16:21:45 +0530

    url

--- LINKTOKEN/urls.py
+++ LINKTOKEN/urls.py
@@ -3,5 +3,5 @@ from .views import LinkToken, webhook_handler
 
 urlpatterns = [
     path('linktoken/', LinkToken.as_view(), name='linktoken'),
-    #path('linktoken/webhook/', webhook_handler, name='webhook_handler'),
+    path('linktoken/webhook/', webhook_handler, name='webhook_handler'),
 ]
