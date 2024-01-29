ca4fa4585c9cb99b1520c79b5b6f16a5a59eaa22
AmiteshSahay <amitesh.sahay@getkloo.com>
2024-01-29 18:04:38 +0530

    removed unwanted py modules

--- merge_integration/helper_functions.py
+++ merge_integration/helper_functions.py
@@ -1,6 +1,5 @@
 import logging
-from marshmallow import ValidationError
-from flask import jsonify, make_response
+
 
 file = "apiLog"
 
@@ -9,24 +8,3 @@ def api_log(level=logging.DEBUG, msg=""):
     logging.basicConfig(level=level, filename=file, filemode='a', format='%(asctime)s [%(levelname)s] %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p', encoding='utf-8')
     logging.log(level, msg)
-
-
-def error_response(error, message='', status_code=500):
-    """Error response"""
-    response = {'success': False, 'error': str(error)}
-    if message != '':
-        response['message'] = message
-
-    if isinstance(error, ValidationError):
-        return make_response(jsonify(response), 400)
-
-    return make_response(jsonify(response), status_code)
-
-
-def success_response(data, message='', status_code=200):
-    """Success response"""
-    response = {'success': True, 'data': data}
-    if message != '':
-        response['message'] = message
-
-    return make_response(jsonify(response), status_code)
