250928d42f4444b0af9d93cde6b8ddd17441c169
Dhirajgiri <dhiraj.giri@blenheimchalcot.com>
2024-01-30 15:10:10 +0530

    added mysql also added insert code into linktoken

--- LINKTOKEN/views.py
+++ LINKTOKEN/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from merge_integration.utils import create_merge_client
 import json
+from .models import ErpLinkToken
 
 class LinkToken(APIView):
     def post(self, request):
@@ -27,10 +28,38 @@ class LinkToken(APIView):
 def webhook_handler(request):
     # Print the request body for debugging
     print("Request Body:", request.body)
+
     try:
-        data = json.loads(request.body)
-        print("Webhook Data:", data)
-        response_data = {"message": "Webhook received and processed successfully", "data": data}
+        #data = json.loads(request.body)
+        #print("Webhook Data:", data)
+
+        payload = json.loads(request.body)
+        linked_account_data = payload.get('linked_account', {})
+        data = payload.get('data', {})
+        linked_account_data = data.get('linked_account')
+        account_token = data.get('data')
+
+        link_token_record = ErpLinkToken(
+            id=linked_account_data.get('id'),
+            org_id=linked_account_data.get('id'),
+            entity_id=linked_account_data.get('id'),
+            link_token=account_token.get('account_token'),
+            integration_name=linked_account_data.get('integration'),
+            magic_link_url=linked_account_data.get('webhook_listener_url'),
+            categories=linked_account_data.get('category'),
+            platform=linked_account_data.get('account_type'),
+            end_user_email_address=linked_account_data.get('end_user_email_address'),
+            end_user_organization_name=linked_account_data.get('end_user_organization_name'),
+            link_expiry_mins=60,
+            should_create_magic_link_url=False,
+            status=linked_account_data.get('status'),
+        )
+        link_token_record.save()
+
+        # Your webhook handling logic here
+
+        # Assuming your logic was successful, respond with data
+        response_data = {"message": "Webhook received and processed successfully"}
         return JsonResponse(response_data, status=200)
     except json.JSONDecodeError as e:
         print("JSON Decode Error:", str(e))
@@ -39,4 +68,4 @@ def webhook_handler(request):
     except Exception as e:
         print("Webhook Handling Error:", str(e))
         # If there's an error in your webhook handling logic, respond with an error using HttpResponse
-        return HttpResponse("Error processing webhook", status=500, content_type='text/plain') 
+        return HttpResponse("Error processing webhook", status=500, content_type='text/plain')
