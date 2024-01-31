a5214aa0af2c26e8b2ae4ca7ebe89d489b8c76b3
zshansiddiquieBCIndia <138130619+zshansiddiquieBCIndia@users.noreply.github.com>
2024-01-31 15:22:21 +0530

    Removing setting from linktoken

--- merge_integration/settings.py
+++ merge_integration/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': get_db_password(os.getenv('RDS_HOST')),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('RDS_HOST'),
         'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
