b988aa4c35e306d0a3e1d3ccd237b36e8a2082bb
zshansiddiquieBCIndia <138130619+zshansiddiquieBCIndia@users.noreply.github.com>
2024-02-01 01:45:40 +0530

    Adding custom response header in the API for CORS

--- merge_integration/settings.py
+++ merge_integration/settings.py
@@ -40,14 +40,30 @@ CORS_ALLOWED_ORIGINS = [
     'https://dev.getkloo.com', # Replace with your React app's origin
 ]
 
-# settings.py
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'https://dev.getkloo.com',
+]
 
 CORS_ALLOW_METHODS = [
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
 ]
 
 
@@ -63,9 +79,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'ACCOUNTS',
     'LINKTOKEN',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +91,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'merge_integration.urls'
