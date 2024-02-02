ea1792ad3a4be533d53c83fa6f2a492f9e0d37ca
AmiteshSahay <amitesh.sahay@getkloo.com>
2024-02-02 13:46:11 +0530

    corrected the GET method

--- COMPANY_INFO/views.py
+++ COMPANY_INFO/views.py
@@ -11,17 +11,19 @@ from merge.resources.accounting import CompanyInfoListRequestExpand, CompanyInfo
 
 class MergeCompanyInfo(APIView):
     @staticmethod
-    def get(_):
-        api_log(msg="Processing GET request in MergeAccounts...")
+    def get_company_info():
         comp_client = Merge(base_url=settings.BASE_URL, account_token=settings.ACCOUNT_TOKEN, api_key=settings.API_KEY)
 
         try:
             organization_data = comp_client.accounting.company_info.list(expand=CompanyInfoListRequestExpand.ADDRESSES)
+            return organization_data
         except Exception as e:
             api_log(
-                msg=f"Error retrieving organizations details: {str(e)} - Status Code: {status.HTTP_500_INTERNAL_SERVER_ERROR}: {traceback.format_exc()}")
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                msg=f"Error retrieving organizations details: {str(e)} \
+                 - Status Code: {status.HTTP_500_INTERNAL_SERVER_ERROR}: {traceback.format_exc()}")
 
+    @staticmethod
+    def build_response_payload(organization_data):
         formatted_addresses = [
             {
                 "type": addr.type,
@@ -32,16 +34,14 @@ class MergeCompanyInfo(APIView):
                 "country_subdivision": addr.country_subdivision,
                 "country": addr.country,
                 "zip_code": addr.zip_code,
-                "created_at": addr.created_at,
-                "modified_at": addr.modified_at,
+                "created_at": addr.created_at.isoformat() + "Z",
+                "modified_at": addr.modified_at.isoformat() + "Z",
             }
             for addr in organization_data.results[0].addresses
-            # Assuming there is at least one organization in the results
         ]
 
         formatted_data = []
         for organization in organization_data.results:
-
             formatted_entry = {
                 "id": organization.id,
                 "remote_id": organization.remote_id,
@@ -51,27 +51,38 @@ class MergeCompanyInfo(APIView):
                 "fiscal_year_end_month": organization.fiscal_year_end_month,
                 "fiscal_year_end_day": organization.fiscal_year_end_day,
                 "currency": organization.currency,
-                "remote_created_at": organization.remote_created_at,
+                "remote_created_at": organization.remote_created_at.isoformat() + "Z",
                 "urls": organization.urls,
-                "addresses": formatted_addresses,  # Include the formatted addresses here
+                "addresses": formatted_addresses,
                 "phone_numbers": [
                     {
                         "number": phone.number,
                         "type": phone.type,
-                        "created_at": phone.created_at,
-                        "modified_at": phone.modified_at,
+                        "created_at": organization.created_at.isoformat() + "Z",
+                        "modified_at": organization.modified_at.isoformat() + "Z",
                     }
                     for phone in organization.phone_numbers
                 ],
                 "remote_was_deleted": organization.remote_was_deleted,
-                "created_at": organization.created_at,
-                "modified_at": organization.modified_at,
+                "created_at": organization.created_at.isoformat() + "Z",
+                "modified_at": organization.modified_at.isoformat() + "Z",
                 "field_mappings": organization.field_mappings,
                 "remote_data": organization.remote_data,
             }
             formatted_data.append(formatted_entry)
-        api_log(msg=f"FORMATTED DATA: {formatted_data} - Status Code: {status.HTTP_200_OK}: {traceback.format_exc()}")
-        return Response(formatted_data, status=status.HTTP_200_OK)
+            kloo_format_json = {"companies": formatted_data}
+
+        return kloo_format_json
+
+    def get(self, request, *args, **kwargs):
+            api_log(msg="Processing GET request in MergeAccounts...")
+
+            organization_data = self.get_company_info()
+            formatted_data = self.build_response_payload(organization_data)
+
+            api_log(msg=f"FORMATTED DATA: {formatted_data} \
+             - Status Code: {status.HTTP_200_OK}: {traceback.format_exc()}")
+            return Response(formatted_data, status=status.HTTP_200_OK)
 
 
 class MergeCompanyDetails(APIView):
