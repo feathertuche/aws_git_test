d5c6890986848ea8fb21eafc126a7ee3055fa267
AmiteshSahay <amitesh.sahay@getkloo.com>
2024-02-06 14:29:46 +0530

    added more commits

--- COMPANY_INFO/views.py
+++ COMPANY_INFO/views.py
@@ -1,5 +1,6 @@
 import requests
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from merge_integration import settings
@@ -7,15 +8,21 @@ from merge.client import Merge
 import traceback
 from merge_integration.helper_functions import api_log
 from merge.resources.accounting import CompanyInfoListRequestExpand, CompanyInfoRetrieveRequestExpand
+from SYNC.views import ListAccountTokenView
+from merge_integration.utils import create_merge_client
 
 
 class MergeCompanyInfo(APIView):
     @staticmethod
-    def get_company_info():
-        comp_client = Merge(base_url=settings.BASE_URL, account_token=settings.ACCOUNT_TOKEN, api_key=settings.API_KEY)
+    def get_company_info(request):
+        get_token = ListAccountTokenView.as_view()
+        queryset = get_token(Request(request=request)).data
+        account_token = queryset[0]['account_token'] if queryset else None
+        account_token_client = create_merge_client(account_token)
+        # comp_client = Merge(base_url=settings.BASE_URL, account_token=settings.ACCOUNT_TOKEN, api_key=settings.API_KEY)
 
         try:
-            organization_data = comp_client.accounting.company_info.list(expand=CompanyInfoListRequestExpand.ADDRESSES)
+            organization_data = account_token_client.accounting.company_info.list(expand=CompanyInfoListRequestExpand.ADDRESSES)
             return organization_data
         except Exception as e:
             api_log(
@@ -93,8 +100,7 @@ class MergeCompanyDetails(APIView):
                                 api_key=settings.API_KEY)
 
         try:
-            organization_data = comp_id_client.accounting.company_info.retrieve(id=id,
-                                                                                 expand=CompanyInfoRetrieveRequestExpand.ADDRESSES)
+            organization_data = comp_id_client.accounting.company_info.retrieve(id=id, expand=CompanyInfoRetrieveRequestExpand.ADDRESSES)
 
             formatted_addresses = [
                 {
