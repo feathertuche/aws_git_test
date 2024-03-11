f67916008f58e505a85719700fef577d17a88513
NabaJyotiBorah <Nabajyoti.Borah@blenheimchalcot.com>
2024-03-11 16:53:21 +0530

    database env chnage

--- merge_integration/settings.py
+++ merge_integration/settings.py
@@ -135,35 +135,35 @@ WSGI_APPLICATION = "merge_integration.wsgi.application"
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": get_db_password(os.getenv("RDS_HOST")),
-#         "HOST": os.getenv("RDS_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#         "OPTIONS": {
-#             "charset": "utf8mb4",
-#         },
-#     }
-# }
-
-
-## Dev
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "myrdssql01",
-        "USER": "masterkloo",
-        "PASSWORD": "ptKJDHJTXd3J0kYF",
-        "HOST": "host.docker.internal",
-        "PORT": 3308,
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": get_db_password(os.getenv("RDS_HOST")),
+        "HOST": os.getenv("RDS_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         "OPTIONS": {
             "charset": "utf8mb4",
         },
     }
 }
+
+
+## Dev
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "myrdssql01",
+#         "USER": "masterkloo",
+#         "PASSWORD": "ptKJDHJTXd3J0kYF",
+#         "HOST": "host.docker.internal",
+#         "PORT": 3308,
+#         "OPTIONS": {
+#             "charset": "utf8mb4",
+#         },
+#     }
+# }
 
 # stage
 # DATABASES = {
