c2fd141e384e97498f8474872b2168c29318951d
AmiteshSahay <amitesh.sahay@getkloo.com>
2024-03-15 13:59:39 +0530

    modified SQL query to include default org_id for link journey

--- LINKTOKEN/helper_function.py
+++ LINKTOKEN/helper_function.py
@@ -8,17 +8,18 @@ def get_org_entity(organization_id):
     with connection.cursor() as cursor:
         cursor.execute(
             """SELECT
-        soe.id
+        soe.*
         FROM organization_configurations soc1
         JOIN organization_entity_details soe ON soe.organization_configurations_id = soc1.id
         WHERE 1 = 1
         AND soc1.organization_id = %s
         AND soc1.config_key_name = 'org_entities'
-        AND soe.`status` IS NULL
-        AND soe.deleted_at IS NULL""",
+        AND soe.deleted_at IS NULL
+        ORDER BY CASE WHEN soe.status = 'default' THEN 0 ELSE 1 END, soe.id ASC""",
             [organization_id],
         )
         row = cursor.fetchone()
+        print("@@@@", row)
         return row
 
 
