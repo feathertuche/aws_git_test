2faf59c54f26b1f56d3af1098d46e53038196f5e
AmiteshSahay <amitesh.sahay@getkloo.com>
2024-04-17 18:23:12 +0530

    revert batch size

--- ACCOUNTS/views.py
+++ ACCOUNTS/views.py
@@ -184,19 +184,19 @@ class InsertAccountData(APIView):
 
                 # Sending data in the batch of 100
                 # account_response_data = None
-                batch_size = accounts_batch_size
-                api_log(msg=f"[BATCH SIZE]:: {batch_size}")
-                for batch in range(0, 58, batch_size):
-                    print(batch)
-                    api_log(msg=f"[BATCH SIZE]:: {batch_size}")
-                    api_log(msg=f"[BATCH]:: {batch}")
-                    batch_data = account_payload[batch:batch + batch_size]
-                    api_log(msg=f"[BATCH DATA]:: {batch_data}")
-                    account_response_data = requests.post(
-                        account_url,
-                        json=batch_data,
-                        # stream=True,
-                    )
+                # batch_size = accounts_batch_size
+                # api_log(msg=f"[BATCH SIZE]:: {batch_size}")
+                # for batch in range(0, 58, batch_size):
+                #     print(batch)
+                #     api_log(msg=f"[BATCH SIZE]:: {batch_size}")
+                #     api_log(msg=f"[BATCH]:: {batch}")
+                #     batch_data = account_payload[batch:batch + batch_size]
+                #     api_log(msg=f"[BATCH DATA]:: {batch_data}")
+                account_response_data = requests.post(
+                    account_url,
+                    json=account_payload,
+                    # stream=True,
+                )
 
                 if account_response_data.status_code == status.HTTP_201_CREATED:
                     api_log(msg="data inserted successfully in the kloo account system")
