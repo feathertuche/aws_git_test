fe1ef4a7f94af545e043282ae1f2913908170b0f
NabaJyotiBorah <Nabajyoti.Borah@blenheimchalcot.com>
2024-04-23 12:54:42 +0530

    added request logger to handle reqeust logs

--- merge_integration/helper_functions.py
+++ merge_integration/helper_functions.py
@@ -1,52 +1,85 @@
+"""
+This module is used to configure the logger
+"""
+
 import logging
 import os
 from datetime import datetime
 
 
-def setup_logging(level=logging.DEBUG):
-    # Get the current date
-    current_date = datetime.now().strftime("%Y-%m-%d")
+# pylint: disable=logging-fstring-interpolation
+class DailyLogger:
+    """
+    This class is used to create a logger object that will be used to log
+    """
 
-    log_folder = "logs"
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
+    def __init__(self, log_folder):
+        """
+        This method is used to initialize the DailyLogger class
+        :param log_folder:  The folder where the logs will be stored
+        """
+        self.log_folder = log_folder
+        os.makedirs(self.log_folder, exist_ok=True)
 
-    log_file = os.path.join(log_folder, f"apiLog_{current_date}.log")
+    def configure_logger(self, logger_name, log_file, enable_console=False):
+        """
+        This method is used to configure the logger
+        :param logger_name: The name of the logger
+        :param log_file: The name of the log file
+        :return:
+        """
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+        log_path = os.path.join(self.log_folder, log_file)
+        handler = logging.FileHandler(log_path)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-    logging.basicConfig(
-        level=level,
-        filename=log_file,
-        filemode="a",
-        format="%(asctime)s [%(levelname)s] : %(filename)s:%(lineno)d - %(message)s",
-        datefmt="%m/%d/%Y %I:%M:%S %p",
-        encoding="utf-8",
-    )
+        # console handler
+        # Optionally add a console handler
+        if enable_console:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.DEBUG)
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
 
-    # Create a console handler
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level)
+        return logger
 
-    # Create a formatter for the console handler
-    formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)s] : %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p"
-    )
-    console_handler.setFormatter(formatter)
 
-    # Create a separate logger for the console output
-    console_logger = logging.getLogger("console")
-    console_logger.propagate = False
-    console_logger.addHandler(console_handler)
+# Create log folder for the current day if it doesn't exist
+current_date = datetime.now().strftime("%Y-%m-%d")
+log_folder_name = os.path.join("logs", current_date)
+
+
+# Create an instance of the DailyLogger class
+logger_object = DailyLogger(log_folder_name)
+
+# Configure logger for API logs
+request_logger = logger_object.configure_logger("request_logger", "request.log")
+api_logger = logger_object.configure_logger(
+    "api_logger", "api.log", enable_console=True
+)
 
 
 def api_log(level=logging.DEBUG, msg=""):
-    # Get the console logger
-    console_logger = logging.getLogger("console")
+    """
+    This function is used to log messages to the API log file
+    :param level: The log level
+    :param msg: The message to log
+    :return:
+    """
+    api_logger.log(level, msg)
 
-    # Log the message to the console
-    console_logger.log(level, msg)
 
-    # Log the message to the file
-    logging.log(level, msg)
-
-
-setup_logging()
+def request_log(level=logging.DEBUG, msg=""):
+    """
+    This function is used to log messages to the request log file
+    :param level: The log level
+    :param msg: The message to log
+    :return:
+    """
+    request_logger.log(level, msg)
--- merge_integration/views.py
+++ merge_integration/views.py
@@ -1,11 +1,18 @@
 from django.http import JsonResponse
 
+from merge_integration.helper_functions import api_log, request_log
+
 
 def health_check(request):
     # Perform any necessary health check logic
     # You can check database connectivity, external dependencies, etc.
     # Return a JSON response indicating the health status
     # For health check
+
+    api_log(msg="HEALTHCHECK: Performing health check")
+    request_log(
+        msg=f"HEALTHCHECK: Request received for health check from {request.META.get('REMOTE_ADDR')}"
+    )
 
     health_status = {
         "status": "ok",
