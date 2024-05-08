6bcf5fa364a044ee89de22eea5deb7a71bab8786
Dhirajgiri <dhiraj.giri@blenheimchalcot.com>
2024-05-08 11:34:35 +0530

    added sqs

--- sqs_utils/sqs_manager.py
+++ sqs_utils/sqs_manager.py
@@ -19,6 +19,8 @@ def send_data_to_queue(data_array):
         region_name=settings.AWS_DEFAULT_REGION,
     )
     sqs_client = session.client("sqs")
+    sqs_client.large_payload_support = "dev-bulk-data-import"
+    sqs_client.use_legacy_attribute = False
     queue_name = settings.SQS_QUEUE
     queue_url = sqs_client.get_queue_url(QueueName=queue_name)
 
