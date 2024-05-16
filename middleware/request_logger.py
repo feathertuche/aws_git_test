39be1bfc0f0df6be220fa6cb973a024699781a78
zshansiddiquieBCIndia <138130619+zshansiddiquieBCIndia@users.noreply.github.com>
2024-05-17 00:02:34 +0530

    Update request_logger.py

--- middleware/request_logger.py
+++ middleware/request_logger.py
@@ -1,15 +1,11 @@
-"""
-Middleware to log `*/api/*` requests and responses.
-"""
-
 import logging
 import socket
 import time
+import json
 
 from merge_integration.helper_functions import request_log
 
 request_logger = logging.getLogger(__name__)
-
 
 class RequestLogMiddleware:
     """Request Logging Middleware."""
@@ -25,17 +21,17 @@ class RequestLogMiddleware:
         response_time = response_start_time - request_start_time
         upstream_time = response_start_time - request_end_time
 
-        log_data = {
-            "remote_address": request.META["REMOTE_ADDR"],
-            "server_hostname": socket.gethostname(),
-            "request_method": request.method,
-            "request_path": request.get_full_path(),
-            "request_headers": dict(request.headers),
-            "response_status": response.status_code,
-            "response_headers": dict(response.items()),
-            "response_time": f"{response_time:.6f} ",
-            "upstream_time": f"{upstream_time:.6f} ",
-        }
+        log_data = [
+            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}]",
+            request.META["REMOTE_ADDR"],
+            request.method,
+            request.get_full_path(),
+            json.dumps(dict(request.headers)),
+            response.status_code,
+            json.dumps(dict(response.items())),
+            f"{response_time:.6f}",
+            f"{upstream_time:.6f}",
+        ]
 
         request_log(msg=log_data)
 
