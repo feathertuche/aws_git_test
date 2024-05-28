aa8d19ca7b175245438971619393752972ebd0a7
NabaJyotiBorah <Nabajyoti.Borah@blenheimchalcot.com>
2024-05-28 16:30:50 +0530

    added script to insert contacts

--- CONTACTS/management/commands/contact_insert_module.py
+++ CONTACTS/management/commands/contact_insert_module.py
@@ -1,7 +1,6 @@
 import requests
 from django.core.management.base import BaseCommand
 from merge.resources.accounting import (
-    ContactsRetrieveRequestExpand,
     ContactsListRequestExpand,
 )
 from rest_framework import status
@@ -38,54 +37,42 @@ class Command(BaseCommand):
 
         account_token = ""
         erp_link_token_id = ""
-        auth_token = ""
         org_id = ""
+        auth_token = ""
 
         try:
             contacts_client = create_merge_client(account_token)
 
+            contacts = []
+
             contact_data = contacts_client.accounting.contacts.list(
                 expand=ContactsListRequestExpand.ADDRESSES,
-                # remote_fields="status",
-                # is_supplier=True,
+                remote_fields="status",
+                is_supplier=True,
                 show_enum_origins="status",
                 page_size=100,
                 include_remote_data=True,
             )
             while True:
                 api_log(msg=f"Adding {len(contact_data.results)} contacts to the list.")
-                for contact in contact_data.results:
-                    api_log(
-                        msg=f"[CONTACT name LIST] : {contact.name} and {contact.id}"
-                    )
+
+                contacts.extend(contact_data.results)
 
                 if contact_data.next is None:
                     break
 
                 contact_data = contacts_client.accounting.contacts.list(
                     expand=ContactsListRequestExpand.ADDRESSES,
-                    # remote_fields="status",
-                    # is_supplier=True,
+                    remote_fields="status",
+                    is_supplier=True,
                     show_enum_origins="status",
                     page_size=100,
                     include_remote_data=True,
                     cursor=contact_data.next,
                 )
 
-            contact_ids = []
+            api_log(msg=f"Total Contacts: {len(contacts)}")
 
-            api_log(msg=f"Total Contacts: {contact_ids}")
-
-            contacts = []
-            for contact_id in contact_ids:
-                contact = contacts_client.accounting.contacts.retrieve(
-                    id=contact_id,
-                    expand=ContactsRetrieveRequestExpand.ADDRESSES,
-                    remote_fields="status",
-                    show_enum_origins="status",
-                    include_remote_data=True,
-                )
-                contacts.append(contact)
             formatted_data = format_contact_data(contacts)
             contact_payload = formatted_data
             contact_payload["erp_link_token_id"] = erp_link_token_id
@@ -96,7 +83,6 @@ class Command(BaseCommand):
                 json=contact_payload,
                 headers={"Authorization": f"Bearer {auth_token}"},
             )
-            api_log(msg="13")
 
             if contact_response_data.status_code == status.HTTP_201_CREATED:
                 api_log(msg="14")
@@ -112,7 +98,7 @@ class Command(BaseCommand):
             return
 
 
-def format_contact_data(contact_data):
+def format_contact_data(contact_data: list):
     formatted_data = []
 
     for contact in contact_data:
