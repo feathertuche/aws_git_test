4fd62ec7ac409c436717d60146f2f29d4e93bf05
amiteshBC <147368189+amiteshBC@users.noreply.github.com>
2024-07-01 13:01:11 +0530

    Update signals.py

--- merge_integration/signals.py
+++ merge_integration/signals.py
@@ -7,5 +7,8 @@ from .tasks import start_polling
 
 @receiver(post_migrate)
 def start_sqs_polling():
+    """
+    Start SQS polling
+    """
     api_log(msg="Starting SQS polling")
     start_polling()
