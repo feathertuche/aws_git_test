de4cef5004948e95ba5e9e8af451e763798c0389
amiteshBC <147368189+amiteshBC@users.noreply.github.com>
2024-08-27 13:37:15 +0530

    MODULR-12012 Added new status for pending status (#451)
    * added new status for pending status
    
    * Update jira_integration.yml
    
    * Update jira_integration.yml
    
    * Update jira_integration.yml
    
    ---------
    
    Co-authored-by: AmiteshSahay <amitesh.sahay@getkloo.com>
    Co-authored-by: zshansiddiquieBCIndia <138130619+zshansiddiquieBCIndia@users.noreply.github.com>

--- INVOICES/management/commands/create_pending_invoice_module.py
+++ INVOICES/management/commands/create_pending_invoice_module.py
@@ -92,6 +92,8 @@ class Command(BaseCommand):
         Returns:
             return_type: None
         """
+        api_log(msg="*********** Calling PROCESS INVOICE bloc to execute NEW invoices ***********")
+
         from INVOICES.models import InvoiceAttachmentLogs
         from INVOICES.models import CronRetry
 
@@ -128,10 +130,10 @@ class Command(BaseCommand):
                             api_log(msg=f"problem type:: {problem_type}")
                         else:
                             problem_type = None
-                            api_log(msg=f"problem type not found")
+                            api_log(msg="problem type not found")
 
                         # Determine status based on the extracted problem_type
-                        if problem_type in ['RATE_LIMITED', 'TIMED_OUT']:
+                        if problem_type in ['RATE_LIMITED', 'The read operation timed out']:
                             error_status = 'pending'
                         else:
                             error_status = 'failed'
@@ -167,13 +169,14 @@ class Command(BaseCommand):
         """
         helper function to set cron time for different types of exception
         """
+        api_log(msg=" ******** This is a HANDLE INVOICE RESPONSE bloc ********")
 
         if invoice_payload["model"]["problem_type"] == "RATE_LIMITED":
-            api_log(msg="setting cron execution time in table for 'RATE_LIMITED'")
-            self.schedule_retry(invoice_payload, timedelta(minutes=5))
-        elif invoice_payload["model"]["problem_type"] == "TIMED_OUT":
-            api_log(msg="setting cron execution time in table for 'MISSING_PERMISSION'")
-            self.schedule_retry(invoice_payload, timedelta(minutes=5))
+            api_log(msg="setting cron execution time in table for 'RATE LIMITED'")
+            self.schedule_retry(invoice_payload, timedelta(hours=1))
+        elif invoice_payload["model"]["problem_type"] == "The read operation timed out":
+            api_log(msg="setting cron execution time in table for 'OPERATION TIMED OUT'")
+            self.schedule_retry(invoice_payload, timedelta(minutes=15))
         else:
             api_log(msg=f"Error: Invoice {invoice_payload['model']['kloo_invoice_id']} not in 'RATE_LIMITED' or "
                         f"'TIMED_OUT'")
@@ -190,18 +193,30 @@ class Command(BaseCommand):
             return_type: None
         """
 
+        api_log(msg=" ******** This is a SCHEDULE RETRY bloc ********")
+
+        from django.db.models import ObjectDoesNotExist
         from INVOICES.models import CronRetry
         retry_at = datetime.now() + retry_delay
         kloo_invoice_id = invoice_payload['model']['kloo_invoice_id']
-        retry_entry, created = CronRetry.objects.update_or_create(
-            id=uuid.uuid1(),
-            kloo_invoice_id=kloo_invoice_id,
-            defaults={'cron_execution_time': retry_at}
-        )
-        if created:
-            api_log(msg=f"New retry scheduled for invoice {kloo_invoice_id} at {retry_at}")
-        else:
+
+        try:
+            # Try to retrieve the existing row
+            cron_retry = CronRetry.objects.get(kloo_invoice_id=kloo_invoice_id)
+            cron_retry.cron_execution_time = retry_at
+            cron_retry.save()
             api_log(msg=f"Updated retry time for invoice {kloo_invoice_id} to {retry_at}")
+        except ObjectDoesNotExist:
+            # create a new row in CRON table
+            CronRetry.objects.create(
+                id=uuid.uuid4(),
+                kloo_invoice_id=kloo_invoice_id,
+                cron_execution_time=retry_at
+            )
+            api_log(msg=f"Created new retry for invoice {kloo_invoice_id} at {retry_at}")
+        except Exception as e:
+            api_log(
+                msg=f"There is some problem with creating or updating CRON table for:: {kloo_invoice_id} at {retry_at} with exception {str(e)}")
 
     def retry_invoices_from_api(self, payload: list):
         """
@@ -213,29 +228,101 @@ class Command(BaseCommand):
         Returns:
             return_type: None
         """
+        api_log(msg="*********** Calling retry bloc executing invoices matching in DB ***********")
+        from INVOICES.models import InvoiceAttachmentLogs
         from INVOICES.models import CronRetry
-        retries = CronRetry.objects.filter(cron_execution_time__lte=datetime.now())
 
-        for retry in retries:
+        # Read the table and fetch all rows
+        read_invoice_id = CronRetry.objects.filter(cron_execution_time__lte=datetime.now())
+        invoices = []
+        pattern = r"'problem_type': '([^']+)'"
+
+        # Running a loop over the payload and compare the invocie_ids with DB invoice_ids
+        try:
             for invoice_payload in payload:
-                api_log(msg=f"api payload in retry invoice function:: {invoice_payload}")
-                api_log(msg=f"DB table kloo ID:: {retry.kloo_invoice_id}")
-                if invoice_payload['model']['kloo_invoice_id'] == retry.kloo_invoice_id:
-                    try:
+                for invoice_ids in read_invoice_id:
+                    if payload["model"]["kloo_invoice_id"] == invoice_ids.kloo_invoice_id:
                         response = self.create_invoice(invoice_payload)
-                        api_log(msg=f"Response from create invoice in RETRY INVOICES method :=> {response}")
-                        api_log(msg=f"Response DATA from create invoice in RETRY INVOICES method :=> {response.data}")
-                        retry.delete()
-                        api_log(msg="Retry deleted successfully")
-                        if response:
-                            self.handle_invoice_response(invoice_payload)
-                    except Exception as e:
-                        retry.cron_execution_time = datetime.now() + timedelta(minutes=5)
-                        retry.save()
-                        api_log(msg=f"Error retrying invoice {invoice_payload['model']['kloo_invoice_id']}: {str(e)}")
-                    except TimeoutError:
-                        retry.cron_execution_time = datetime.now() + timedelta(minutes=5)
-                        retry.save()
+                        if response.status_code == status.HTTP_201_OK:
+                            api_log(msg=f"Response from create invoice in RETRY INVOICES method :=> {response}")
+                            api_log(
+                                msg=f"Response DATA from create invoice in RETRY INVOICES method :=> {response.data}")
+
+                            # delete the successful invoice entry from the table
+                            invoice_ids.delete()
+                        else:
+                            # When an Invoice fails.
+                            api_log(msg=f"string type response:: {str(response)}")
+                            api_log(msg=f"Original response:: {response}")
+
+                            # Extract the status code using regex
+                            status_code_match = re.search(r'status_code:\s*(\d+)', str(response))
+                            status_code = int(status_code_match.group(1)) if status_code_match else None
+                            api_log(msg=f"STATUS CODE in retry_invoices_from_api method:::: {status_code}")
+
+                            if status_code:
+                                if status_code in [404, 400]:
+                                    api_log(
+                                        msg=f"Error creating invoice: status_code: {status_code}, body: {response}")
+
+                                    invoice_id = invoice_payload['model']['kloo_invoice_id']
+                                    api_log(msg=f"invoice ID in retry_invoices_from_api method:: {invoice_id}")
+
+                                    # Extract problem_type using regex
+                                    match = re.search(pattern, response['error'])
+                                    if match:
+                                        problem_type = match.group(1)
+                                        api_log(msg=f"problem type:: {problem_type}")
+                                    else:
+                                        problem_type = None
+                                        api_log(msg=f"problem type not found")
+
+                                    # Determine status based on the extracted problem_type
+                                    if problem_type in ['RATE_LIMITED', 'The read operation timed out']:
+                                        error_status = 'pending'
+                                    else:
+                                        error_status = 'failed'
+
+                                    api_log(
+                                        msg=f"Response in the retry_invoices_from_api function in management command "
+                                            f"file::: {response} and "
+                                            f"Invoice_id is {invoice_id}")
+
+                                    # Append the status to the invoices list
+                                    invoices.append({
+                                        'id': invoice_id,
+                                        'status': error_status
+                                    })
+
+                                    # Update problem_type in InvoiceAttachmentLogs
+                                    update_invoice_id = str(invoice_id)
+                                    api_log(msg=f"update invoice id: {update_invoice_id}")
+                                    new_invoice_id = "".join(update_invoice_id.split("-"))
+                                    api_log(msg=f"new invoice id: {new_invoice_id}")
+                                    log = InvoiceAttachmentLogs.objects.filter(invoice_id=new_invoice_id).first()
+                                    if log:
+                                        log.problem_type = problem_type
+                                        log.save()
+                                    api_log(msg=f"log problem type column value::: {log.problem_type}")
+                                    self.handle_invoice_response(invoice_payload)
+
+                                    if error_status == 'failed':
+                                        invoice_ids.delete()
+                                        api_log(msg=f"Deleted failed invoice entry with invoice_id: {invoice_id}")
+
+                            # Log or return the invoices JSON
+                        final_update = {"invoices": invoices}
+                        api_log(msg=f"final update JSON: {final_update}")
+                        self.post_response(final_update)
+
+        except requests.exceptions.RequestException as e:
+            api_log(msg=f"A request exception occurred in RETRY INVOICES method: {str(e)}")
+
+        except AttributeError as e:
+            api_log(msg=f"An attribute error occurred in RETRY INVOICES method: {str(e)}")
+
+        except Exception as e:
+            api_log(msg=f"An error occurred in RETRY INVOICES method: {str(e)}")
 
     def post_response(self, response: dict):
         """
@@ -260,8 +347,6 @@ class Command(BaseCommand):
             if pending_invoice_response.status_code in [200, 201]:
                 api_log(msg=f"Successfully UPDATED the response code: {response}")
             else:
-                api_log(
-                    msg=f"Failed to update response code:::: {response}. Status Code:::: {pending_invoice_response.status_code}")
+                api_log(msg=f"Failed to update Laravel api due to Status Code:::: {pending_invoice_response.status_code}")
         except Exception as e:
             api_log(msg=f"Exception occurred while posting invoices: {str(e)}")
-
