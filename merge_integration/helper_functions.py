e60393644ed1b03e60c5fd54b210df7f408d7255
zshansiddiquieBCIndia <138130619+zshansiddiquieBCIndia@users.noreply.github.com>
2024-09-04 14:16:15 +0530

    Update helper_functions.py

--- merge_integration/helper_functions.py
+++ merge_integration/helper_functions.py
@@ -1,13 +1,9 @@
-"""
-This module is used to configure the logger
-"""
-
 import logging
 import os
 from datetime import datetime
+import boto3
+import watchtower
 
-
-# pylint: disable=logging-fstring-interpolation
 class DailyLogger:
     """
     This class is used to create a logger object that will be used to log
@@ -21,59 +17,99 @@ class DailyLogger:
         self.log_folder = log_folder
         os.makedirs(self.log_folder, exist_ok=True)
 
-    def configure_logger(self, logger_name, log_file, enable_console=False):
+        # Set up CloudWatch logs client
+        self.cloudwatch_log_group_name = os.getenv('REQUEST_CLOUDWATCH_LOG_GROUP', 'your-default-log-group')
+        self.boto3_logs_client = boto3.client("logs", region_name=os.getenv('AWS_DEFAULT_REGION', 'us-west-2'))
+
+    def configure_logger(self, logger_name, log_file=None, enable_console=False, enable_cloudwatch=False):
         """
         This method is used to configure the logger
         :param logger_name: The name of the logger
-        :param log_file: The name of the log file
-        :return:
+        :param log_file: The name of the log file (optional)
+        :param enable_console: Boolean to enable console logging
+        :param enable_cloudwatch: Boolean to enable CloudWatch logging
+        :return: Configured logger object
         """
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.DEBUG)
-        log_path = os.path.join(self.log_folder, log_file)
-        handler = logging.FileHandler(log_path)
-        handler.setLevel(logging.DEBUG)
+        
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
         )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
 
-        # console handler
-        # Optionally add a console handler
+        # File handler (Only if log_file is provided)
+        if log_file:
+            log_path = os.path.join(self.log_folder, log_file)
+            file_handler = logging.FileHandler(log_path)
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+        # Console handler
         if enable_console:
             console_handler = logging.StreamHandler()
             console_handler.setLevel(logging.DEBUG)
             console_handler.setFormatter(formatter)
             logger.addHandler(console_handler)
 
+        # CloudWatch handler
+        if enable_cloudwatch:
+            try:
+                cloudwatch_handler = watchtower.CloudWatchLogHandler(
+                    boto3_client=self.boto3_logs_client,
+                    log_group_name=self.cloudwatch_log_group_name,
+                    stream_name=logger_name,
+                    use_queues=False
+                )
+                cloudwatch_handler.setLevel(logging.DEBUG)
+                cloudwatch_handler.setFormatter(formatter)
+                logger.addHandler(cloudwatch_handler)
+            except Exception as e:
+                logger.error(f"Failed to set up CloudWatch logging: {e}")
+
         return logger
 
+    def log_message(self, logger, level, msg, api_path=None):
+        """
+        Log a message
+        :param logger: The logger to use
+        :param level: The log level
+        :param msg: The message to log
+        :param api_path: The API path, if applicable
+        """
+        logger.log(level, msg)
+        if api_path and '/api/erp/health/' in api_path:
+            logger.info(f"Logged message for API path: {api_path}")
 
-# Create log folder for the current day if it doesn't exist
+# Example Usage
 current_date = datetime.now().strftime("%Y-%m-%d")
 log_folder_name = os.path.join("logs", current_date)
 
-
-# Create an instance of the DailyLogger class
 logger_object = DailyLogger(log_folder_name)
 
-# Configure logger for API logs
-request_logger = logger_object.configure_logger("request_logger", "request.log")
-api_logger = logger_object.configure_logger(
-    "api_logger", "api.log", enable_console=True
+request_logger = logger_object.configure_logger(
+    "request_logger", 
+    log_file=None,  # Skip file logging
+    enable_console=False, 
+    enable_cloudwatch=True
 )
 
+api_logger = logger_object.configure_logger(
+    "api_logger", 
+    log_file="api.log",  # File logging enabled for API logger
+    enable_console=True, 
+    enable_cloudwatch=True
+)
 
-def api_log(level=logging.DEBUG, msg=""):
+def api_log(level=logging.DEBUG, msg="", api_path=None):
     """
     This function is used to log messages to the API log file
     :param level: The log level
     :param msg: The message to log
+    :param api_path: The API path, if applicable
     :return:
     """
-    api_logger.log(level, msg)
-
+    logger_object.log_message(api_logger, level, msg, api_path)
 
 def request_log(level=logging.DEBUG, msg=""):
     """
